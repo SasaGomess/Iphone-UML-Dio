82ef7bf50b4e4e71fe26e2219afa221b0940fb7b
Sabrina Gomes <sabrina.gomess.dev@gmail.com>
2025-04-07 10:01:11 -0300

    s

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,8 +7,6 @@ import services.ReprodutorMusical;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static Scanner sc = new Scanner(System.in);
